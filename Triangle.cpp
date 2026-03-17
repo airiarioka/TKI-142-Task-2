697f629927775aee604cdd73f765c2889544f142
Денис Шматок <shmatok2007@mail.ru>
2026-03-17 15:52:46 +0300

    Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -14,7 +14,7 @@ Triangle::Triangle(const Point& p1, const Point& p2, const Point& p3)
     AB=getSide(a,b);
     BC=getSide(b,c);
     AC=getSide(a,c);
-    if ( AB<=0 || BC<=0||AC<=0||AB>=BC+AC||BC>=AB+AC||AC>=AB+BC)
+    if ( (( (p3.getX()-p1.getX())/(p2.getX()-p1.getX()) ) == ( (p3.getY()-p1.getY())/(p2.getY()-p1.getY()) ) || same(p1,p2)||same(p1,p3) || same(p2,p3) ) || (AB<=0||BC<=0||AC<=0||AB>=BC+AC||BC>=AB+AC||AC>=AB+BC))
     {
         cout<<"Error 2\n";
         exit(1);
